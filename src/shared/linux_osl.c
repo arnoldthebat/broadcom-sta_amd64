9e211753e02185650e1bf21f0dab68ee9578d94f
Tony Ditchfield <tony.ditchfield@gmail.com>
2018-01-09 21:27:04 +0000

    Hack3

--- src/shared/linux_osl.c
+++ src/shared/linux_osl.c
@@ -1080,7 +1080,7 @@ osl_os_get_image_block(char *buf, int len, void *image)
 	if (!image)
 		return 0;
 
-	rdlen = kernel_read(fp, &fp->f_pos, buf, len);
+	rdlen = kernel_read(fp, &fp->f_pos, buf, &len);
 	if (rdlen > 0)
 		fp->f_pos += rdlen;
 
